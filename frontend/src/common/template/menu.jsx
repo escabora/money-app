5281b67d4a5a53c6c4d86ba0230117e7171648ec
Desenvolvedor Telhanorte <desenvolvedortelhanorte@MacBook-Air-de-Desenvolvedor.local>
2019-12-26 14:25:16 -0200

    [front] refactoring routes with IndexRoute

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -4,9 +4,9 @@ import MenuTree from './menuTree'
 
 export default props => (
   <ul className='sidebar-menu'>
-    <MenuItem path='#/..' label='dashboard' icon='dashboard' />
+    <MenuItem path='/' label='dashboard' icon='dashboard' />
     <MenuTree label='Cadastro' icon='edit'>
-      <MenuItem path='#billingCycles'
+      <MenuItem path='billingCycles'
           label='Ciclos de Pagamentos' icon='usd' />
     </MenuTree>
   </ul>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,8 +8,8 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
+import Routes from './main/routes'
 import reducers from './main/reducers'
-import App from './main/app'
 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
@@ -17,7 +17,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Routes />
     </Provider>
 , document.getElementById('app'))
 
